5f90659bf35e669b4dbb495aeebed7e7bbc92dd7
elciCode <elcira.rpi@gmail.com>
2022-11-16 17:56:34 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'photos-app',
+  webDir: 'dist/photos-app',
+  bundledWebRuntime: false
+};
+
+export default config;
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailComponent } from './pages/detail/detail.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'detail',
+    component: DetailComponent
+  },
+  {
+    path: 'gallery',
+    component: GalleryComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
